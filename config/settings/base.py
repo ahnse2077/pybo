94639d8d49bdd4eb7cfef43ccae740764302cf02
안상은 <ahnse2077@gmail.com>
2021-08-12 18:24:50 +0900

    error메세지

--- config/settings/base.py
+++ config/settings/base.py
@@ -129,55 +129,6 @@ LOGIN_REDIRECT_URL = '/'
 # 로그아웃시 이동하는 URL
 LOGOUT_REDIRECT_URL = '/'
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse',
-        },
-        'require_debug_true': {
-            '()': 'django.utils.log.RequireDebugTrue',
-        },
-    },
-    'formatters': {
-        'django.server': {
-            '()': 'django.utils.log.ServerFormatter',
-            'format': '[{server_time}] {message}',
-            'style': '{',
-        }
-    },
-    'handlers': {
-        'console': {
-            'level': 'INFO',
-            'filters': ['require_debug_true'],
-            'class': 'logging.StreamHandler',
-        },
-        'django.server': {
-            'level': 'INFO',
-            'class': 'logging.StreamHandler',
-            'formatter': 'django.server',
-        },
-        'mail_admins': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
-        }
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['console', 'mail_admins'],
-            'level': 'INFO',
-        },
-        'django.server': {
-            'handlers': ['django.server'],
-            'level': 'INFO',
-            'propagate': False,
-        },
-    }
-}
-
-
 
 # ---------------------------------- [edit] ---------------------------------- #
 # 로깅설정
@@ -197,46 +148,6 @@ LOGGING = {
             '()': 'django.utils.log.ServerFormatter',
             'format': '[{server_time}] {message}',
             'style': '{',
-        }
-    },
-    'handlers': {
-        'console': {
-            'level': 'INFO',
-            'filters': ['require_debug_true'],
-            'class': 'logging.StreamHandler',
-        },
-        'django.server': {
-            'level': 'INFO',
-            'class': 'logging.StreamHandler',
-            'formatter': 'django.server',
-        },
-        'mail_admins': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
-        }
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['console', 'mail_admins'],
-            'level': 'INFO',
-        },
-        'django.server': {
-            'handlers': ['django.server'],
-            'level': 'INFO',
-            'propagate': False,
-        },
-    }
-}
-# ---------------------------------------------------------------------------- #
-
-LOGGING = {
-    (... 생략 ...)
-    'formatters': {
-        'django.server': {
-            '()': 'django.utils.log.ServerFormatter',
-            'format': '[{server_time}] {message}',
-            'style': '{',
         },
 # ---------------------------------- [edit] ---------------------------------- #
         'standard': {
@@ -244,11 +155,6 @@ LOGGING = {
         },
 # ---------------------------------------------------------------------------- #
     },
-    (... 생략 ...)
-}
-
-LOGGING = {
-    (... 생략 ...)
     'handlers': {
         'console': {
             'level': 'INFO',
@@ -277,11 +183,6 @@ LOGGING = {
         },
 # ---------------------------------------------------------------------------- #
     },
-    (... 생략 ...)
-}
-
-LOGGING = {
-    (... 생략 ...)
     'loggers': {
         'django': {
 # ---------------------------------- [edit] ---------------------------------- #
@@ -295,17 +196,4 @@ LOGGING = {
             'propagate': False,
         },
     }
-}
-
-LOGGING = {
-   (... 생략 ...)
-    'loggers': {
-        (... 생략 ...)
-# ---------------------------------- [edit] ---------------------------------- #
-        'pybo': {
-            'handlers': ['console', 'file'],
-            'level': 'INFO',
-        },
-# ---------------------------------------------------------------------------- #
-    },
 }
