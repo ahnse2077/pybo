ab9e2573fb0dbe1f1d28f5c86148eb81835fa291
안상은 <ahnse2077@gmail.com>
2021-08-13 11:08:24 +0900

    DB 생성

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,6 +1,18 @@
 from .base import *
 
-ALLOWED_HOSTS = ['3.38.84.114']
+ALLOWED_HOSTS = ['3.38.84.114', 'e-plate.co.kr']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
+# ---------------------------------- [edit] ---------------------------------- #
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'pybo',
+        'USER': 'dbmasteruser',
+        'PASSWORD': 'ahnse0512#,
+        'HOST': 'ls-af6a858f9e519c300628cf64837cc9bbd782f0aa.cudjjnwh8xnw.us-east-1.rds.amazonaws.com',
+        'PORT': '5432',
+    }
+}
+# ---------------------------------------------------------------------------- #
